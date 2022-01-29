02064a6bb66fc0e3f7aeb79a44e0c149e7fc8083
shikelong <578622705@qq.com>
2022-01-29 08:57:36 +0800

    fix: fix localStorage undefined error in server-rendering.2

--- src/utils/useSafeWindow.ts
+++ src/utils/useSafeWindow.ts
@@ -2,5 +2,5 @@ export function useSafeWindow<T>(
   fn: (window: Window) => T | undefined,
   def: T
 ) {
-  return typeof window !== undefined ? fn(window) : def
+  return typeof window !== "undefined" ? fn(window) : def
 }
